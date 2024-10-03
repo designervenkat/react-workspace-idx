16f7c68d10f780cd1d08e264c091badeacbe6b34
@designervenkat <murty.av@gmail.com>
2024-10-03 03:45:47 +0000

    06-react-router-base-setup

--- 06-react-router/src/main.jsx
+++ 06-react-router/src/main.jsx
@@ -1,10 +1,47 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Layout from "./Layout.jsx";
+import HomePage from "./components/pages/Home.page.jsx";
+import AboutPage from "./components/pages/About.page.jsx";
+import ContactPage from "./components/pages/Contact.page.jsx";
+import ProjectPage from "./components/pages/Project.page.jsx";
+
+const navigation = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+      {
+        path: "about",
+        element: <AboutPage />,
+      },
+      {
+        path: "project",
+        element: <ProjectPage />,
+      },
+      {
+        path: "contact",
+        element: <ContactPage />,
+      },
+      {
+        path: "dummy",
+        element: "This is Dummy Component",
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={navigation} />
+  </StrictMode>
+);
