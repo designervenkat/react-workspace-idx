aaa1c28075a5477c686378d7f23210a8bd34592b
@designervenkat <murty.av@gmail.com>
2024-10-29 02:12:56 +0000

    base project setup

---
+++ 08-react-redux/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="h-screen w-full grid place-content-center bg-slate-900 text-slate-400">
+            <h2>React Redux</h2>
+        </div>
+    )
+}
+
+export default App
